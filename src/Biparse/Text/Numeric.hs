f0d39153b1aefb93b0b7c3a170c9489bec84f533
William Rusnack <williamrusnack@gmail.com>
2023-10-05 15:23:49 -0400

    added scientific to numeric

--- src/Biparse/Text/Numeric.hs
+++ src/Biparse/Text/Numeric.hs
@@ -6,18 +6,20 @@ module Biparse.Text.Numeric
   , naturalBaseTen'
   , IntConstrints
   , intBaseTen
+  , scientific
   , RealConstrints
   , realBaseTen
   ) where
 
 import Biparse.Biparser (Iso, IsoClass(iso), split, upon, SubState, SubStateContext, try, ignoreBackward, split, Biparser, ElementContext, comap)
-import Biparse.General (take)
+import Biparse.General (take, optional, takeTri)
 import Biparse.Text (CharElement)
 import Data.Char (isDigit)
 import Data.Int (Int8, Int16, Int32, Int64)
 import Data.Word (Word, Word8, Word16, Word32, Word64)
 import GHC.Float (Float, Double)
-import GHC.Num ((*))
+import GHC.Num ((*), negate, Integer, abs)
+import GHC.Real (Fractional, (^^))
 import Safe (readMay)
 
 instance NaturalConstraints c s m n Word   char => IsoClass c m n s Word   where iso = naturalBaseTen
@@ -26,24 +28,25 @@ instance NaturalConstraints c s m n Word16 char => IsoClass c m n s Word16 where
 instance NaturalConstraints c s m n Word32 char => IsoClass c m n s Word32 where iso = naturalBaseTen
 instance NaturalConstraints c s m n Word64 char => IsoClass c m n s Word64 where iso = naturalBaseTen
 
-type NaturalConstraints c s m n a char =
+type NaturalConstraints c s m n number char =
   ( CharElement c s char
-  , Show a
   , IsSequence (SubState c s)
   , MonadState s m
   , MonadFail m
   , MonadWriter (SubState c s) n
   , MonadFail n
-  , Read a
   , SubStateContext c s
+  , Read number
+  , Num number
+  , Show number
   )
 
-naturalBaseTen :: forall c s m n a char. NaturalConstraints c s m n a char => Iso c m n s a
+naturalBaseTen :: forall c s m n number char. NaturalConstraints c s m n number char => Iso c m n s number
 naturalBaseTen = do
-  ds <- fmap toChar . toList <$> digitsBaseTen
+  ds <- fmap toChar . toList <$> digitsBaseTen `upon` abs
   maybe (fail $ "Could not parse " <> show ds <> " to natural base 10.") pure $ readMay ds
 
-naturalBaseTen' :: forall a c s m n char. NaturalConstraints c s m n a char => Iso c m n s a
+naturalBaseTen' :: forall number c s m n char. NaturalConstraints c s m n number char => Iso c m n s number
 naturalBaseTen' = naturalBaseTen
 
 instance NaturalConstraints c s m n Int   char => IsoClass c m n s Int   where iso = naturalBaseTen
@@ -52,44 +55,57 @@ instance NaturalConstraints c s m n Int16 char => IsoClass c m n s Int16 where i
 instance NaturalConstraints c s m n Int32 char => IsoClass c m n s Int32 where iso = naturalBaseTen
 instance NaturalConstraints c s m n Int64 char => IsoClass c m n s Int64 where iso = naturalBaseTen
 
-type IntConstrints c s m n a char =
-  ( NaturalConstraints c s m n a char
+type IntConstrints c s m n number char =
+  ( NaturalConstraints c s m n number char
   , MonadPlus m
   , Alternative n
-  , Num a
+  , Num number
+  , Ord number
   , ElementContext c s
   )
 
-intBaseTen :: forall c s m n a char. IntConstrints c s m n a char => Iso c m n s a
+intBaseTen :: forall c s m n number char. IntConstrints c s m n number char => Iso c m n s number
 intBaseTen = do
   s <- sign
   n <- naturalBaseTen
-  pure $ s * n
+  pure $ s n
 
-type RealConstrints c s m n a ss char =
-  ( NaturalConstraints c s m n a char
+type RealConstrints c s m n number ss char =
+  ( NaturalConstraints c s m n number char
   , MonadPlus m
   , ElementContext c s
   , Show ss
   , Alternative n
-  , Num a
+  , Num number
+  , Ord number
   , ss ~ SubState c s
   )
 
 instance RealConstrints c s m n Float  ss char => IsoClass c m n s Float  where iso = realBaseTen
 instance RealConstrints c s m n Double ss char => IsoClass c m n s Double where iso = realBaseTen
 
-realBaseTen :: forall c s m n a ss char.
-  ( RealConstrints c s m n a ss char
-  ) => Iso c m n s a
+-- | Only wirtes digits and not powers of 10.
+scientific :: forall c s m n number ss char.
+  ( Fractional number
+  , RealConstrints c s m n number ss char
+  ) => Iso c m n s number
+scientific = do
+  digits <- realBaseTen
+  power :: Maybe Integer <- comap (const Nothing) $ optional do
+    take (fromChar 'E') <|> take (fromChar 'e')
+    intBaseTen
+  pure $ maybe id ((*) . (10 ^^)) power $ digits
+
+realBaseTen :: forall c s m n number ss char.
+  ( RealConstrints c s m n number ss char
+  ) => Iso c m n s number
 realBaseTen = do
   s <- sign
-  ws <- digitsBaseTen :: Biparser c s m n a ss
+  ws <- digitsBaseTen `upon` abs
   ds <- comap (const mempty) $ ignoreBackward
-    $ try (cons <$> (fromChar '.' <$ take (fromChar '.'))  <*> digitsBaseTen)
+    $   try (cons <$> (fromChar '.' <$ take (fromChar '.'))  <*> digitsBaseTen)
     <|> pure mempty
-    :: Biparser c s m n a ss
-  maybe (fail "Could not parse real base 10.") (pure . (* s)) $ readMay $ fmap toChar $ toList $ ws <> ds
+  maybe (fail "Could not parse real base 10.") (pure . s) $ readMay $ fmap toChar $ toList $ ws <> ds
 
 digitsBaseTen :: forall c m n s u ss char.
   ( CharElement c s char
@@ -102,7 +118,19 @@ digitsBaseTen :: forall c m n s u ss char.
   ) => Biparser c s m n u ss
 digitsBaseTen = split (state $ span $ isDigit . toChar) `upon` fromList . fmap fromChar . show
 
-sign :: forall c s m n a ss char.
+data Sign = Positive | Negative | Zero deriving (Show, Eq)
+deduceSign :: forall number.
+  ( Ord number
+  , Num number
+  )
+  => number
+  -> Sign
+deduceSign x
+  | x < 0 = Negative
+  | x > 0 = Positive
+  | otherwise = Zero
+
+sign :: forall c s m n number ss char.
   ( CharElement c s char
   , MonadState s m
   , MonadFail m
@@ -111,9 +139,11 @@ sign :: forall c s m n a ss char.
   , MonadWriter ss n
   , MonadFail n
   , Alternative n
-  , Num a
+  , Num number
+  , Ord number
   , ElementContext c s
   , ss ~ SubState c s
-  ) => Biparser c s m n a a
-sign = comap (const 1) $ ignoreBackward $ (-1) <$ try (take $ fromChar '-') <|> pure 1
+  ) => Biparser c s m n number (number -> number)
+sign = comap deduceSign $ takeTri (fromChar '-') Negative negate <|> pure id
+--sign = comap (const 1) $ ignoreBackward $ (-1) <$ try (take $ fromChar '-') <|> pure 1
 
--- tests/Biparse/Text/NumericSpec.hs
+++ tests/Biparse/Text/NumericSpec.hs
@@ -1,6 +1,8 @@
 module Biparse.Text.NumericSpec where
 
 import Biparse.Text.Numeric
+import GHC.Num (Num)
+import GHC.Real (Fractional)
 
 spec :: Spec
 spec = do
@@ -9,15 +11,8 @@ spec = do
     ()
     ()
     (\f -> do
-      it "success" do
-        f "0"    `shouldBe` Right (0,   Position 1 2 mempty)
-        f "1"    `shouldBe` Right (1,   Position 1 2 mempty)
-        f "123"  `shouldBe` Right (123, Position 1 4 mempty)
-        f "123x" `shouldBe` Right (123, Position 1 4 "x")
-
-      it "fail" do
-        f ""     `shouldSatisfy` errorPosition 1 1
-        f " 123" `shouldSatisfy` errorPosition 1 1
+      naturalsForward f
+      failIntegerForward f
     )
     \b -> do
       it "success" do
@@ -29,61 +24,95 @@ spec = do
     ()
     ()
     (\f -> do
-      it "success" do
-        f "1"     `shouldBe` Right (1,   Position 1 2 mempty)
-        f "123"   `shouldBe` Right (123, Position 1 4 mempty)
-        f "123x"  `shouldBe` Right (123, Position 1 4 "x")
-        f "-1"    `shouldBe` Right (-1,   Position 1 3 mempty)
-        f "-123"  `shouldBe` Right (-123, Position 1 5 mempty)
-        f "-123x" `shouldBe` Right (-123, Position 1 5 "x")
+      describe "success" do
+        naturalsForward f
+        negativeIntegerForward f
 
-      it "fail" do
-        f ""      `shouldSatisfy` errorPosition 1 1
-        f " 123"  `shouldSatisfy` errorPosition 1 1
-        f " -123" `shouldSatisfy` errorPosition 1 1
+      failIntegerForward f
     )
     \b -> do
-      it "success" do
-        b 1      >>= (`shouldBe` (1,    "1"))
-        b 123    >>= (`shouldBe` (123,  "123"))
-        b (-1)   >>= (`shouldBe` (-1,   "-1"))
-        b (-123) >>= (`shouldBe` (-123, "-123"))
+      integerBackward b
+
+  fb @() "scientific"
+    (scientific :: Iso ColumnsOnly (FM Text) IO () () (Position Text) Double)
+    ()
+    ()
+    (\f -> do
+      realForward f
+
+      it "10s power mulitiplier" do
+        f "1e1" `shouldBe` Right (10, Position 1 4 mempty)
+        f "-1.0E-1" `shouldBe` Right (-0.1, Position 1 8 mempty)
+        f "12.345E2" `shouldBe` Right (1234.5, Position 1 9 mempty)
+    )
+    \b -> do
+      realBackward b
 
   fb @() "realBaseTen"
     (realBaseTen :: Iso UnixLC (FM String) IO () () (Position String) Double)
     ()
     ()
-    (\f -> do
-      it "whole" do
-        f "1"    `shouldBe` Right (1,   Position 1 2 mempty)
-        f "123"  `shouldBe` Right (123, Position 1 4 mempty)
-        f "123x" `shouldBe` Right (123, Position 1 4 "x")
+    realForward
+    realBackward
 
-      it "decimal" do
-        f "1.0"     `shouldBe` Right (1.0,     Position 1 4 mempty)
-        f "123.123" `shouldBe` Right (123.123, Position 1 8 mempty)
+naturalsForward :: (Show a1, Show a2, Show text, Eq a1, Eq a2, Eq text, Num a2, Monoid text, IsString t, IsString text) => (t -> Either a1 (a2, Position text)) -> SpecWith ()
+naturalsForward f = it "naturals" do
+  f "0"    `shouldBe` Right (0,   Position 1 2 mempty)
+  f "1"    `shouldBe` Right (1,   Position 1 2 mempty)
+  f "123"  `shouldBe` Right (123, Position 1 4 mempty)
+  f "123x" `shouldBe` Right (123, Position 1 4 "x")
 
-      it "negative" do
-        f "-1.0"     `shouldBe` Right (-1.0,     Position 1 5 mempty)
-        f "-123.123" `shouldBe` Right (-123.123, Position 1 9 mempty)
+negativeIntegerForward :: (Show a1, Show a2, Show text, Eq a1, Eq a2, Eq text, Num a2, Monoid text, IsString t, IsString text) => (t -> Either a1 (a2, Position text)) -> SpecWith ()
+negativeIntegerForward f = it "negative integer" do
+  f "-1"    `shouldBe` Right (-1,   Position 1 3 mempty)
+  f "-123"  `shouldBe` Right (-123, Position 1 5 mempty)
+  f "-123x" `shouldBe` Right (-123, Position 1 5 "x")
 
-      it "fail" do
-        f ""     `shouldSatisfy` errorPosition 1 1
-        f " 123" `shouldSatisfy` errorPosition 1 1
-        f "1."   `shouldSatisfy` errorPosition 1 3
-    )
-    \b -> do
-      it "whole" do
-        b 1   >>= (`shouldBe` (1,   "1.0"))
-        b 123 >>= (`shouldBe` (123, "123.0"))
+failIntegerForward :: (Show b, IsString t) => (t -> Either ErrorPosition b) -> SpecWith ()
+failIntegerForward f = it "fail integer" do
+  f ""      `shouldSatisfy` errorPosition 1 1
+  f " 123"  `shouldSatisfy` errorPosition 1 1
+  f " -123" `shouldSatisfy` errorPosition 1 1
 
-      it "decimal" do
-        b (1.23)   >>= (`shouldBe` (1.23,   "1.23"))
-        b (123.45) >>= (`shouldBe` (123.45, "123.45"))
+integerBackward :: (Show a, Show b, Eq a, Eq b, Num t, Num a, IsString b) => (t -> IO (a, b)) -> SpecWith ()
+integerBackward b = it "integer" do
+  b 0      >>= (`shouldBe` (0,    "0"))
+  b 1      >>= (`shouldBe` (1,    "1"))
+  b 123    >>= (`shouldBe` (123,  "123"))
+  b (-1)   >>= (`shouldBe` (-1,   "-1"))
+  b (-123) >>= (`shouldBe` (-123, "-123"))
 
-      it "negative" do
-        b (-1)      >>= (`shouldBe` (-1,      "-1.0"))
-        b (-123)    >>= (`shouldBe` (-123,    "-123.0"))
-        b (-1.23)   >>= (`shouldBe` (-1.23,   "-1.23"))
-        b (-123.45) >>= (`shouldBe` (-123.45, "-123.45"))
+realForward :: (Show a, Show text, Eq a, Eq text, Monoid text, IsString t, IsString text, Fractional a) => (t -> Either ErrorPosition (a, Position text)) -> Spec
+realForward f = do
+  naturalsForward f
+
+  it "decimal" do
+    f "1.0"     `shouldBe` Right (1.0,     Position 1 4 mempty)
+    f "123.123" `shouldBe` Right (123.123, Position 1 8 mempty)
+
+  it "negative" do
+    f "-1.0"     `shouldBe` Right (-1.0,     Position 1 5 mempty)
+    f "-123.123" `shouldBe` Right (-123.123, Position 1 9 mempty)
+
+  describe "fail" do
+    failIntegerForward f
+
+    it "fail real" do
+      f "1."   `shouldSatisfy` errorPosition 1 3
+
+realBackward :: (Show a, Show b, Eq a, Eq b, IsString b, Fractional t, Fractional a) => (t -> IO (a, b)) -> Spec
+realBackward b = do
+  it "whole" do
+    b 1   >>= (`shouldBe` (1,   "1.0"))
+    b 123 >>= (`shouldBe` (123, "123.0"))
+
+  it "decimal" do
+    b (1.23)   >>= (`shouldBe` (1.23,   "1.23"))
+    b (123.45) >>= (`shouldBe` (123.45, "123.45"))
+
+  it "negative" do
+    b (-1)      >>= (`shouldBe` (-1,      "-1.0"))
+    b (-123)    >>= (`shouldBe` (-123,    "-123.0"))
+    b (-1.23)   >>= (`shouldBe` (-1.23,   "-1.23"))
+    b (-123.45) >>= (`shouldBe` (-123.45, "-123.45"))
 
