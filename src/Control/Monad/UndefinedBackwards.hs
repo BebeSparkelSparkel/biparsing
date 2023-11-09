d8c7b58f6255a693222aa04e0480f0e411b59a43
William Rusnack <williamrusnack@gmail.com>
2023-11-09 12:38:18 -0500

    added returnAbsurd for UndefinedBackwards

--- src/Control/Monad/UndefinedBackwards.hs
+++ src/Control/Monad/UndefinedBackwards.hs
@@ -1,9 +1,15 @@
 {-# OPTIONS_GHC -Wno-missing-methods #-}
 module Control.Monad.UndefinedBackwards
   ( UndefinedBackwards
+  , returnAbsurd
   ) where
 
+import GHC.Err (undefined)
+
 data UndefinedBackwards w a
+
+returnAbsurd :: UndefinedBackwards w a
+returnAbsurd = undefined
 
 instance Functor (UndefinedBackwards w)
 instance Applicative (UndefinedBackwards w)
@@ -12,3 +18,4 @@ instance MonadPlus (UndefinedBackwards w)
 instance MonadFail (UndefinedBackwards w)
 instance Monad (UndefinedBackwards w)
 instance Monoid w => MonadWriter w (UndefinedBackwards w)
+
